da80e8ffdcab8486838d87fdb6f4dda84d97a3b1
lHuanYul <joe2345698@gmail.com>
2025-05-16 01:54:34 +0800

    0516 01a54

--- principal_program/MDK-ARM/Keil-mdk/vec.d
+++
@@ -1,5 +0,0 @@
-keil-mdk\vec.o: ..\Core\Src\principal\vec.c
-keil-mdk\vec.o: ../Core/Inc/principal/vec.h
-keil-mdk\vec.o: C:\Program_HY\Keil_v5\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
-keil-mdk\vec.o: C:\Program_HY\Keil_v5\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdbool.h
-keil-mdk\vec.o: C:\Program_HY\Keil_v5\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\string.h
