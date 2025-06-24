883924478a7e0b183a824fa37794f6c68f1350bd
lHuanYul <joe2345698@gmail.com>
2025-06-24 19:53:31 +0800

    ancillary adding

---
+++ ancillary_program/Core/Inc/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdint.h>
+
+#define TIM2_PSC        170
+#define TIM2_ARR        20000
+
+#define TIM2_CH1_COMP     500
+#define TIM2_CH2_COMP    1500
+#define TIM2_CH3_COMP    2500
+
+extern uint32_t pulse;
