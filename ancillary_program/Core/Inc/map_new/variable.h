f8faca760f683d150eed89744a2c2292a6e7608b
Yuru-saiko <A21507375123@gmail.com>
2025-09-25 16:27:26 +0800

    up

--- ancillary_program/Core/Inc/map_new/variable.h
+++ ancillary_program/Core/Inc/map_new/variable.h
@@ -12,6 +12,7 @@
 #define NO_DATA 0xFF
 #define TIME_INIT 1500
 
+// 三個速度不可相同
 #define MAP_SETPOINT_TRACK     20   // 循跡速度目標
 #define MAP_SETPOINT_ROTATE    15   // 原地旋轉速度目標
 #define MAP_SETPOINT_FALL_BACK 13   // 倒退速度目標
